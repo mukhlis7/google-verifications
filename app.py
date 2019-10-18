aca1f43ff28f4da8e453800f8c8dc6f52a832410
mukhlis7 <39841113+mukhlis7@users.noreply.github.com>
2019-10-18 10:12:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def index():
 		
 	return render_template('index.html')
 
-#if __name__ == "__main__":
-    #app.run(host='0.0.0.0',port='80',debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='80')
